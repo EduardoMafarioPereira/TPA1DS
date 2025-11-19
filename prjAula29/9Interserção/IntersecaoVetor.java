80558243a48c90659a3098c17ea19d347e5c0105
EduardoMafarioPereira <xdudughamer@gmail.com>
2025-11-19 11:16:54 -0300

    Add files via upload

---
+++ prjAula29/9Interserção/IntersecaoVetor.java
@@ -0,0 +1,50 @@
+package prjAula29Vetor;
+import java.util.Scanner;
+public class IntersecaoVetor {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		final int TAM = 10;
+		int i = 0;
+		int Contador = 0;
+		int a[], b[], c[];
+		a = new int [TAM]; b = new int [TAM]; c = new int [TAM];
+		for(i=0;i<TAM;i++) {
+			System.out.println("Insira o "+(i+1)+"º valor");
+			a[i] = ler.nextInt();
+			
+		}
+		for(i=0;i<TAM;i++) {
+			System.out.println("Insira o "+(i+1)+"º valor do segundo vetor");
+			b[i] = ler.nextInt();
+			
+		}
+		
+		System.out.print("A = [ ");
+		for(i=0;i<TAM;i++) {
+			System.out.print(a[i]+" ");
+			
+		}
+		System.out.println("]");
+		
+		System.out.print("B = [ ");
+		for(i=0;i<TAM;i++) {
+			System.out.print(b[i]+" ");
+			
+		}
+		System.out.println("]");
+		
+		System.out.print("C = [ ");
+		for(i=0;	i<TAM;i++) {
+			for(Contador=0;Contador<TAM;Contador++) {
+				if(a[i] == b[Contador]) {
+					c[i] = a[i];
+					System.out.print(c[i]+" ");
+				}
+			}
+		}
+		System.out.println("]");
+		ler.close();
+	}
+
+}
