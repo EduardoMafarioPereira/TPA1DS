09877b8d4bbf7e0822d9ec4e3461864f37610e2b
EduardoMafarioPereira <xdudughamer@gmail.com>
2025-11-19 10:58:34 -0300

    Add files via upload

---
+++ prjAula29/2TabuadaVetor/TabuadaVetor.java
@@ -0,0 +1,30 @@
+package prjAula29Vetor;
+import java.util.Scanner;
+public class TabuadaVetor {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		final int TAM =5; int b= 0 , i = 0, r= 0;
+		int a[];
+		a = new int [TAM];
+		
+		
+		for(i=0;i<TAM;i++) {
+			System.out.println("Insira o "+(i+1)+"º valor");
+			a[i] = ler.nextInt();
+		}
+		for(i=0;i<TAM;i++) {
+			System.out.println(" ");
+			
+			for(b= 1;b<11;b++) {
+				r = a[i] *b;
+				System.out.println(a[i]+" * "+b+" = "+r);
+			}
+		}
+		ler.close();
+		
+		
+		
+	}
+
+}
